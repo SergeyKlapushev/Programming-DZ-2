a5dbff06129e0a9a90a13cdd4a6864300e09aeab
Sergey Klapushev <klapischeff@yandex.ru>
2022-10-05 11:52:16 +0300

    Task1 completed

---
+++ Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Введите число: ");
+
+int n1 = Convert.ToInt32(Console.ReadLine());
+if (n1 >= 100 && n1 < 1000)
+{
+    Console.WriteLine($"Вторая цифра числа {n1}: {n1%100/10}");
+}
+else{Console.WriteLine("Введите число от 100 до 999");}
